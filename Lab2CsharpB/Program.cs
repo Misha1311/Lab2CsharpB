f7b6871a2d2cb1d9552faf50163310934410b6ed
Misha1311 <47081026+Misha1311@users.noreply.github.com>
2020-03-31 18:01:14 +0300

    Добавьте файлы проекта.

---
+++ Lab2CsharpB/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Lab2CsharpB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of quadrangles:");
+            int N = 0;
+            bool isCorrect;
+            do
+            {
+                isCorrect = Int32.TryParse(Console.ReadLine(), out N);
+                if (!isCorrect) { Console.WriteLine("Wrong entering."); }
+            } while (!isCorrect);
+
+            double avgsqure = 0;
+            double min = 0;
+            double max = 0;
+
+            Triangle[] tring = new Triangle[N];
+
+
+            for (int i = 0; i < N; i++)
+            {
+                do
+                {
+                    tring[i] = new Triangle();
+                    tring[i].Lenght();
+                    tring[i].IsExists();
+                    if (tring[i].exist == true)
+                    {
+                        tring[i].Print();
+                        tring[i].Angle();
+                        tring[i].Perimetr();
+                        tring[i].Square();
+                    }
+                } while (tring[i].exist == false);
+
+                min = tring[0].perimetr;
+                avgsqure += tring[i].square;
+                
+                Console.WriteLine("----------------------------------");
+            }
+            avgsqure /= N;
+
+            Console.WriteLine($"Average Square = {avgsqure}");
+
+            for (int i = 0; i < N; i++)
+            {
+                if (tring[i].perimetr < min)
+                {
+                    min = tring[i].perimetr;
+                }
+
+            }
+            Console.WriteLine($"Minimum of perimetr = {min}");
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine("Enter the number of Right Triangle:");
+            int M = 0;
+            do
+            {
+                isCorrect = Int32.TryParse(Console.ReadLine(), out M);
+                if (!isCorrect) { Console.WriteLine("Wrong entering."); }
+            } while (!isCorrect);
+            RightTriangle[] all = new RightTriangle[M];
+
+            for (int i = 0; i < M; i++)
+            {
+                do
+                {
+                    all[i] = new RightTriangle();
+                    all[i].Lenght();
+                    all[i].IsExists();
+                    if (all[i].exist)
+                    {
+                        if (all[i].isRight())
+                        {
+                            all[i].Print();
+                            all[i].Angle();
+                            all[i].Perimetr();
+                            all[i].Square();
+                        }
+                    }
+                } while (!all[i].isRight() || !all[i].exist);
+                Console.WriteLine("----------------------------------");
+
+            }
+            int count=0;
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (all[i].lenght[j] > max)
+                    {
+                        max = all[i].lenght[j];
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Maximum of hypotenuse = {count}");
+
+        }
+    }
+}
---
+++ Lab2CsharpB/Triangle1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab2CsharpB
+{
+    public struct Point2D
+    {
+        public int x;
+        public int y;
+    };
+
+    class Triangle
+    {
+        int n = 3;
+        Point2D[] points;
+        public double[] lenght;
+        double angle1 = 0;
+        double angle2 = 0;
+        double angle3 = 0;
+        public double perimetr = 0;
+        public double square = 0;
+        public bool exist = true;
+        Random rand = new Random();
+
+        public double a;
+        public double b;
+        public double c;
+        public Triangle()
+        {
+
+            points = new Point2D[3];
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i].x = rand.Next(0, 10);
+                points[i].y = rand.Next(0, 10);
+            }
+        }
+
+        public void IsExists()
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (lenght[i] + lenght[(i + 1) % n] <= lenght[(i + 2) % n])
+                {
+                    exist = false;
+                    break;
+                }
+
+            }
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine($"X = {points[i].x} Y  = {points[i].y}");
+            }
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.WriteLine($"Lenght {i + 1} : {lenght[i]}");
+            }
+        }
+
+        public void Lenght()
+        {
+            lenght = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                lenght[i] = Math.Sqrt(Math.Pow(points[(i + 1) % n].x - points[i].x, 2) + Math.Pow(points[(i + 1) % n].y - points[i].y, 2));
+            }
+
+        }
+
+        public void Angle()
+        {
+            a = lenght[0];
+            b = lenght[1];
+            c = lenght[2];
+
+            angle1 = Math.Acos((a * a + c * c - b * b) / (2 * a * c)) * 180 / Math.PI;
+            angle2 = Math.Acos((a * a + b * b - c * c) / (2 * a * b)) * 180 / Math.PI;
+            angle3 = Math.Acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / Math.PI;
+            Console.WriteLine($"Angle 1 = {angle1}");
+            Console.WriteLine($"Angle 2 = {angle2}");
+            Console.WriteLine($"Angle 3 = {angle3}");
+        }
+
+        public void Perimetr()
+        {
+            perimetr = lenght[0] + lenght[1] + lenght[2];
+
+            Console.WriteLine("Perimetr = " + perimetr);
+        }
+
+        public void Square()
+        {
+            square = 0.5 * a * b * Math.Sin(angle1);
+            if (square < 0)
+            {
+                square *= -1;
+            }
+            Console.WriteLine($"Square = {square}");
+
+        }
+    };
+}
